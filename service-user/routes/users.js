aa11ce4c9b3d10a9c64f83ad6ae04600e5971154
alaqsaka <aqsaabidin15@gmail.com>
2022-10-04 19:49:51 +0700

    chore: create user register api

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const usersHandler = require('./handler/users')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// Register route 
+router.post('/register', usersHandler.register)
 
 module.exports = router;
