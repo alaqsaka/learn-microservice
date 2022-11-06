08a9ff8ae918177e532a59f2e04bdc5a873749f8
alaqsaka <aqsaabidin15@gmail.com>
2022-11-06 16:49:11 +0700

    chore: create function helpers

--- service-course/app/helpers.php
+++ service-course/app/helpers.php
@@ -1,6 +1,48 @@
 <?php
 
-function test()
+use Illuminate\Support\Facades\Http;
+
+function getUser($userId)
 {
-    return "just testing";
+    $url = env('SERVICE_USER_URL') . 'users/' . $userId;
+
+    try {
+        $response = Http::timeout(10)->get($url);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        //throw $th;
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'Service user unavailable'
+        ];
+    }
+}
+
+// mendapatkan data user dengan id tertentu
+function getUserByIds($userIds = [])
+{
+    $url = env('SERVICE_USER_URL') . 'users/';
+    try {
+        if (count($userIds) === 0) {
+            return [
+                'status' => 'success',
+                'http_code' => 200,
+                'data' => []
+            ];
+        }
+
+        $response = Http::timeout(10)->get($url, ['user_ids[]' => $userIds]);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'Service user unavailable'
+        ];
+    }
 }
