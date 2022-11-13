fbec1497acd7406b4efc0fd47572da0413c444ab
alaqsaka <aqsaabidin15@gmail.com>
2022-11-13 20:32:46 +0700

    chore: detail course

--- service-course/app/Http/Controllers/CourseController.php
+++ service-course/app/Http/Controllers/CourseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chapter;
 use App\Models\Review;
 use App\Models\Course;
 use App\Models\Mentor;
@@ -47,11 +48,29 @@ class CourseController extends Controller
 
         // mendapatkan review course
         $reviews = Review::where('course_id', '=', $id)->get()->toArray();
+        if (count($reviews) > 0) {
+            $userIds = array_column($reviews, 'user_id');
+            $users = getUserByIds($userIds);
+            // echo "<pre>" . print_r($users, 1) . "</pre>";
+            if ($users['status'] === 'error') {
+                $reviews = [];
+            } else {
+                foreach ($reviews as $key => $review) {
+                    $userIndex = array_search($review['user_id'], array_column($users['data'], 'id'));
+                    $reviews[$key]['users'] = $users['data'][$userIndex];
+                }
+            }
+        }
 
         // mendapatkan jumlah student yang mengambil course
         $totalStudent = MyCourse::where('course_id', '=', $id)->count();
 
+        $totalVideos = Chapter::where('course_id', '=', $id)->withCount('lessons')->get()->toArray();
+
+        $finalTotalVideos = array_sum(array_column($totalVideos, 'lessons_count'));
+
         $course['reviews'] = $reviews;
+        $course['total_videos'] = $finalTotalVideos;
         $course['total_student'] = $totalStudent;
 
         return response()->json([
